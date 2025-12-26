4313ebbf3d55930d19219fe1e56248f9ce763ea5
KuboniZA <sipumzo.lucwaba@gmail.com>
2025-12-26 20:03:26 +0200

    Initial letter layout with minimal CSS.

--- src/components/LogoView.vue
+++ src/components/LogoView.vue
@@ -1,19 +1,114 @@
 <script setup lang="ts">
 
-
 </script>
 
 <template>
-  <div>
+  <div class="logo-container">
     <pre>
-    .------.            _     _            _    _            _
-    |A_  _ |.          | |   | |          | |  (_)          | |
-    |( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
-    | \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
-    |  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   <
-    `-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
-          |  \/ K|                            _/ |
-          `------'                           |__/
+    .------.
+    |A_  _ |.
+    |( \/ ).-----.
+    | \  /|K /\  |
+    |  \/ | /  \ |
+    `-----| \  / |
+          |  \/ K|
+          `------'
+    </pre>
+    <pre class="character">
+       _
+      | |
+      | |__
+      | '_ \
+      | |_) |
+      |_.__/
+    </pre>
+    <pre class="character">
+       _
+      | |
+      | |
+      | |
+      | |
+      |_|
+    </pre>
+    <pre class="character">
+
+
+
+      __ _
+     / _` |
+    | (_| |
+     \__,_|
+    </pre>
+    <pre class="character">
+
+
+
+       ___
+      / __|
+     | (__
+      \___|
+    </pre>
+    <pre class="character">
+       _
+      | |
+      | | __
+      | |/ /
+      |   <
+      |_|\_\
+    </pre>
+    <pre class="character">
+         _
+        (_)
+         _
+        | |
+        | |
+        | |
+       _/ |
+      |__/
+    </pre>
+    <pre class="character">
+
+
+
+      __ _
+     / _` |
+    | (_| |
+     \__,_|
+    </pre>
+    <pre class="character">
+
+
+
+       ___
+      / __|
+     | (__
+      \___|
+    </pre>
+    <pre class="character">
+       _
+      | |
+      | | __
+      | |/ /
+      |   <
+      |_|\_\
     </pre>
   </div>
 </template>
+
+<style scoped>
+  .logo-container {
+    height: 7rem;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    position: absolute;
+    justify-content: center;
+    align-items: center;
+  }
+  .character {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+  }
+</style>
