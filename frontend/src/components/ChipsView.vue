9936e97dde21d7ddeda2251660074ab335a83578
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-01-28 17:57:29 +0200

    Create first chip.

--- frontend/src/components/ChipsView.vue
+++ frontend/src/components/ChipsView.vue
@@ -3,6 +3,16 @@
 <template>
   <div class="chips-container">
     <div class="container-mask" />
+
+    <div id="rand1">
+      <div class="horizontal-line" />
+      <div class="vertical-line" />
+      <div class="left-leaning-line" />
+      <div class="right-leaning-line" />
+      <div class="inner-mask">
+        <span class="chip">1</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,5 +36,64 @@
   border-top-right-radius: 25px;
   border-bottom-left-radius: 25px;
   box-shadow: inset 4px -4px 8px rgba(0, 0, 0, 0.4);
+}
+#rand1 {
+  height: 5rem;
+  width: 5rem;
+  border: 2px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  position: absolute;
+  left: 1.5rem;
+  top: 3rem;
+  background-color: greenyellow;
+  box-shadow: -4px 4px 8px rgba(0, 0, 0, 0.7);
+  overflow: hidden;
+}
+.inner-mask {
+  height: 3.5rem;
+  width: 3.5rem;
+  border: 2px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: white;
+  box-shadow: inset -2px 4px 8px rgba(0, 0, 0, 0.4);
+  z-index: 5;
+}
+.chip {
+  font-size: 2rem;
+  font-weight: bold;
+}
+.horizontal-line {
+  position: absolute;
+  width: 5rem;
+  height: 0.5rem;
+  background-color: black;
+}
+.vertical-line {
+  position: absolute;
+  width: 0.5rem;
+  height: 5rem;
+  background-color: black;
+}
+.left-leaning-line {
+  position: absolute;
+  width: 0.5rem;
+  height: 7.07rem;
+  border-radius: 25px;
+  background-color: black;
+  transform: rotate(-45deg);
+}
+.right-leaning-line {
+  position: absolute;
+  width: 0.5rem;
+  height: 7.07rem;
+  border-radius: 25px;
+  background-color: black;
+  transform: rotate(45deg);
 }
 </style>
