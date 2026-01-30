9456439d340cb42fbe80607bb9529deb57dbed56
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-01-30 12:10:19 +0200

    Add High-Roller logo and change background to black.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ChipsView from "./components/ChipsView.vue";
+import TableView from "./components/TableView.vue";
 </script>
 
 <template>
   <ChipTest />
+  <TableView />
   <ChipsView />
   <RouterView />
 </template>
---
+++ frontend/src/components/TableView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-container">
+    <h1 class="text">HIGH ROLLER</h1>
+  </div>
+</template>
+
+<style scoped>
+.text-container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  place-items: center;
+  z-index: -1000;
+  position: absolute;
+}
+.text {
+  font-size: 16rem;
+  background: linear-gradient(to right, rgb(247, 210, 2), rgb(1, 202, 182));
+  background-clip: text;
+  color: transparent;
+}
+</style>
