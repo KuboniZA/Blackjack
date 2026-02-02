f0ea3d38f999ce2b868532cc3b93e31cca935a44
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-02-03 00:37:59 +0200

    Add soem responsiveness with media queries.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 // import ChipsView from "./components/ChipsView.vue";
+// import AceView from "./components/Clubs/AceView.vue";
 import TableView from "./components/TableView.vue";
 // import TestView from "./components/TestView.vue";
 </script>
 
 <template>
   <TableView />
-  <ChipsView />
-  <TestView />
+  <!-- <AceView /> -->
+  <!-- <ChipsView /> -->
+  <!-- <TestView /> -->
   <RouterView />
 </template>
 
--- frontend/src/components/TableView.vue
+++ frontend/src/components/TableView.vue
@@ -1,23 +1,51 @@
 <template>
   <div class="text-container">
-    <h1 class="text">HIGH ROLLER</h1>
+    <h1 class="shimmer">
+      <span class="text" id="row1">HIGH</span><span class="text" id="row2">ROLLER</span>
+    </h1>
   </div>
 </template>
 
 <style scoped>
 .text-container {
   width: 100%;
-  height: 100vh;
-  display: flex;
+  height: 100dvh;
+  display: grid;
+  grid-template-columns: 2 (1fr);
   justify-content: center;
   place-items: center;
   z-index: -1000;
   position: absolute;
 }
-.text {
-  font-size: 16rem;
+.shimmer {
   background: linear-gradient(to right, rgb(247, 210, 2), rgb(1, 202, 182));
   background-clip: text;
   color: transparent;
+}
+.text {
+  font-size: clamp(5rem, 11vw + 1rem, 18rem);
+  padding: 1.75rem;
+}
+
+@media screen and (width <= 900px) {
+  .text-container {
+    width: 100dvw;
+    grid-template-rows: 2 (1fr);
+  }
+  .text {
+    background: linear-gradient(to right, rgb(247, 210, 2), rgb(1, 202, 182));
+    background-clip: text;
+    color: transparent;
+    justify-content: center;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  #row1 {
+    top: 30%;
+  }
+  #row2 {
+    top: 50%;
+  }
 }
 </style>
