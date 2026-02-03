c96f03c17db21324f7a2f72840ebcd0a2a77701d
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-02-03 20:38:14 +0200

    Ace of Spades complete.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 // import ChipsView from "./components/ChipsView.vue";
 
+import AceOfSpades from "./components/Spades/AceOfSpades.vue";
 import TableView from "./components/TableView.vue";
 // import TestView from "./components/TestView.vue";
 </script>
 
 <template>
   <TableView />
-
+<AceOfSpades
   <!-- <ChipsView /> -->
   <!-- <TestView /> -->
   <RouterView />
