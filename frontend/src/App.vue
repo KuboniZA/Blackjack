2ac10048fb0522c07f8f929fdd8bd97a7848b7ee
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-02-03 19:35:14 +0200

    Suit of Hearts complete.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 // import ChipsView from "./components/ChipsView.vue";
-import AceOfHearts from "./components/Hearts/AceOfHearts.vue";
+
 import TableView from "./components/TableView.vue";
 // import TestView from "./components/TestView.vue";
 </script>
 
 <template>
   <TableView />
-  <AceOfHearts />
+
   <!-- <ChipsView /> -->
   <!-- <TestView /> -->
   <RouterView />
