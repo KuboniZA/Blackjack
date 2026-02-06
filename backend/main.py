2f928414ad653af931520e637b910fbeeef30a09
KuboniZA <sipumzo.lucwaba@gmail.com>
2026-02-06 19:27:16 +0200

    Create venv and initialise fastapi into main.py

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,10 @@
-import pygame
+from fastapi import FastAPI
 from blackjack_engine import GameEngine
 
-pygame.init()
+app = FastAPI()
 
 game = GameEngine()
 
-WIDTH, HEIGHT = 1000, 800
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Blackjack")
-GREEN = (53, 101, 77)
-
-def main():
-    run = True
-
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                break
-        WIN.fill(GREEN)
-        pygame.display.flip()
-
-    pygame.quit()
-
-if __name__ == "__main__":
-    main()
+@app.get("/one-deck-game")
+def get_cards():
+    return {"first_deal": game.new_game_state()}
